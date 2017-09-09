c236e5bf16b26b25d2015479dc8bef25783f39ad
Antonioni Campos <antonioni.campos@outlook.com>
2017-09-09 20:07:57 -0300

    Mudança no construtor e adição de getNumbers()

--- jogo.py
+++ jogo.py
@@ -1,7 +1,10 @@
 class Jogo:
 
-	def __init__(self):
+	def __init__(self, numeros=[], numConcurso=0):
 		# numeros: os 6 numeros sorteados num dado concurso
-		self.numeros = []
+		self.numeros = numeros
 		# numConcurso: numero do concurso
-		self.numConcurso = 0
+		self.numConcurso = numConcurso
+
+	def getNumbers(self):
+		return self.numeros
