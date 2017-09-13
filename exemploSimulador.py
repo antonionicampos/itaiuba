30d647307d00a6b0dfd795a91dc9bca38eabae7a
Bruno <bruno.sant.a@gmail.com>
2017-09-13 14:16:53 -0300

    Modificação do codigo, incluindo a utilizacao do metodo update

--- exemploSimulador.py
+++ exemploSimulador.py
@@ -7,12 +7,16 @@ Aposta1 = Jogo(numeros=[1,2,3,4,5,6])
 Aposta2 = Jogo(numeros=[1,2,3,4,5,6,7])
 
 Apostas = Jogos(listaJogos=[Aposta1, Aposta2])
-simulacao = Simulador(jogos=Apostas)
+simulacao = Simulador(apostas=Apostas)
+
+def att():
+	#Aqui entra os codigos para atualizar as apostas para simulacao
+	simulacao.setApostas(Apostas)
 
 print('Ultimo jogo da lista de jogos a serem "sorteados": ')
-print(simulacao.getSorteios().getListaJogos()[0])
+print(simulacao.getSorteios().getListaJogos()[-1])
 print('Ultimo jogo da lista considerada como banco de dados - historico: ')
-print(simulacao.getHistorico().getListaJogos()[0])
+print(simulacao.getHistorico().getListaJogos()[-1])
 
 print('Aposta 1: ')
 print(simulacao.getVolantes()[0].getAposta().getNumeros())
@@ -21,3 +25,10 @@ print('Valor R$ {:.2f}'.format(simulacao.getVolantes()[0].getPreco()))
 print('Aposta 2: ')
 print(simulacao.getVolantes()[1].getAposta().getNumeros())
 print('Valor R$ {:.2f}'.format(simulacao.getVolantes()[1].getPreco()))
+
+simulacao.update(func=att)
+
+print('Ultimo jogo da lista de jogos a serem "sorteados": ')
+print(simulacao.getSorteios().getListaJogos()[-1])
+print('Ultimo jogo da lista considerada como banco de dados - historico: ')
+print(simulacao.getHistorico().getListaJogos()[-1])
